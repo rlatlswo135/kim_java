d05464ccd4e5fedb2ae9ebc2139d4ee869a4bf90
rlatlswo135 <rlatlswo13@gmail.com>
2023-10-26 23:40:41 +0900

    interfaace

--- temp/src/ch07/InterfaceExample/InterfaceExample.java
+++ temp/src/ch07/InterfaceExample/InterfaceExample.java
@@ -1,20 +1,40 @@
 package ch07.InterfaceExample;
 
 interface RemoteControl {
-    public void turnOn();
+    int BATTERY = 100;
+
+    void turnOn();
+
+    default void setMute(Boolean mute){
+        if(mute){
+            System.out.println("장치를 음소거합니다.");
+        }else{
+            System.out.println("장치 음소거를 해제합니다.");
+        }
+    };
+
+    static void turnOff(){
+        System.out.println("장치 전원을 끕니다.");
+    };
 }
 
 class Television implements RemoteControl {
     @Override
     public void turnOn(){
-        System.out.println("tv를 켭니다.");
+        System.out.println("tv를 켭니다." + "남은 배터리 : " + RemoteControl.BATTERY);
+    }
+
+    @Override
+    public void setMute(Boolean mute){ // 기본(default)메서드 재정의 -> 반드시 public을 붙이고 default를 떼주어야한다
+        System.out.println("장치 음소거를 해제합니다.");
     }
 }
 
 class Audio implements RemoteControl{
     @Override
     public void turnOn(){
-        System.out.println("오디오를 켭니다.");
+        // NOTICE : 정적 변수에 접근할때는 정적방식으로 접근해야한다 = 클래스에서 참조하는 방식 (this,인스턴스가 아닌)
+        System.out.println("오디오를 켭니다." + "남은 배터리 : " + RemoteControl.BATTERY);
     }
 }
 
@@ -25,5 +45,8 @@ public class InterfaceExample  {
 
         rc = new Audio();
         rc.turnOn();
+
+        RemoteControl.turnOff(); // 정적(static) 메서드
+
     }
 }
