e9c7a1d98a399cdcd0cef08fb4e4603ed89c20b4
rlatlswo135 <rlatlswo13@gmail.com>
2024-02-05 23:30:02 +0900

    oop2

--- oop_basic/app/src/main/java/oop_basic/Calculator.java
+++ oop_basic/app/src/main/java/oop_basic/Calculator.java
@@ -2,6 +2,17 @@ package oop_basic;
 
 public class Calculator {
     static int calculate(int arg1, String operator, int arg2) {
-        return Operator.calculate(arg1, operator, arg2);
+        switch (operator) {
+            case "+":
+                return arg1 + arg2;
+            case "-":
+                return arg1 - arg2;
+            case "*":
+                return arg1 * arg2;
+            case "/":
+                return arg1 / arg2;
+            default:
+                return arg1;
+        }
     }
 }
